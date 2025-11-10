eb3c3642753e4744e681d198ba501958d52697cb
Malu <hello@beeancy.com>
2025-11-10 03:25:50 +0000

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  define: {
-    // This makes the environment variables available to the client-side code
-    // Cloudflare Pages will substitute `process.env.API_KEY` with the value you set in the dashboard
-    'process.env.API_KEY': JSON.stringify(process.env.API_KEY),
-    'process.env.SERPER_API_KEY': JSON.stringify(process.env.SERPER_API_KEY),
-  }
 })
